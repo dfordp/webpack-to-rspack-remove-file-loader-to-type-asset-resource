f32213540b1d2d5fdf8846859925a5cd0a3b354e
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-10-16 23:35:40 +0530

    added test case

--- src/index.ts
+++ src/index.ts
@@ -5,50 +5,74 @@ export default function transform(file, api, options) {
 
   // Find all objects within the rules array
   root.find(j.ObjectExpression).forEach((path) => {
-    const properties = path.node.properties;
+      const properties = path.node.properties;
 
-    // Check if the object has a 'use' property
-    const useProperty = properties.find(
-      (prop) =>
-      j.ObjectProperty.check(prop) &&
-      j.Identifier.check(prop.key) &&
-      prop.key.name === 'use',
-    );
+      // Check if the object has a 'use' property
+      const useProperty = properties.find(
+          (prop) =>
+              j.ObjectProperty.check(prop) &&
+              j.Identifier.check(prop.key) &&
+              prop.key.name === 'use',
+      );
 
-    if (useProperty) {
-      // Check if 'use' is an array with 'file-loader' as a string or object
-      const useValue = useProperty.value;
-      if (j.ArrayExpression.check(useValue)) {
-        const hasFileLoader = useValue.elements.some(
-          (element) =>
-          (j.StringLiteral.check(element) &&
-            element.value === 'file-loader') ||
-          (j.ObjectExpression.check(element) &&
-            element.properties.some(
-              (prop) =>
+      // Check if the object has a 'loader' property
+      const loaderProperty = properties.find(
+          (prop) =>
               j.ObjectProperty.check(prop) &&
               j.Identifier.check(prop.key) &&
               prop.key.name === 'loader' &&
               j.StringLiteral.check(prop.value) &&
               prop.value.value === 'file-loader',
-            )),
-        );
+      );
 
-        if (hasFileLoader) {
-          // Remove 'use' property and add 'type: asset/resource'
+      if (useProperty) {
+          // Check if 'use' is an array with 'file-loader' as a string or object
+          const useValue = useProperty.value;
+          if (j.ArrayExpression.check(useValue)) {
+              const hasFileLoader = useValue.elements.some(
+                  (element) =>
+                      (j.StringLiteral.check(element) &&
+                          element.value === 'file-loader') ||
+                      (j.ObjectExpression.check(element) &&
+                          element.properties.some(
+                              (prop) =>
+                                  j.ObjectProperty.check(prop) &&
+                                  j.Identifier.check(prop.key) &&
+                                  prop.key.name === 'loader' &&
+                                  j.StringLiteral.check(prop.value) &&
+                                  prop.value.value === 'file-loader',
+                          )),
+              );
+
+              if (hasFileLoader) {
+                  // Remove 'use' property and add 'type: asset/resource'
+                  path.node.properties = properties.filter(
+                      (prop) => prop !== useProperty,
+                  );
+                  path.node.properties.push(
+                      j.objectProperty(
+                          j.identifier('type'),
+                          j.stringLiteral('asset/resource'),
+                      ),
+                  );
+                  dirtyFlag = true;
+              }
+          }
+      }
+
+      if (loaderProperty) {
+          // Remove 'loader' property and add 'type: asset/resource'
           path.node.properties = properties.filter(
-            (prop) => prop !== useProperty,
+              (prop) => prop !== loaderProperty,
           );
           path.node.properties.push(
-            j.objectProperty(
-              j.identifier('type'),
-              j.stringLiteral('asset/resource'),
-            ),
+              j.objectProperty(
+                  j.identifier('type'),
+                  j.stringLiteral('asset/resource'),
+              ),
           );
           dirtyFlag = true;
-        }
       }
-    }
   });
 
   return dirtyFlag ? root.toSource() : undefined;
